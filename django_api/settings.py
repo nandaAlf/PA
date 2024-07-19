7f45102b49946bdb0a7f4d8608b4592d923c2995
fernandaAlfonso <alfonsonanda03@gmail.com>
2024-07-19 19:42:56 -0400

    postgreDB conected

--- django_api/settings.py
+++ django_api/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AP1',
+        'USER':'postgres',
+        'PASSWORD':'mfap.0330',
+        'HOST':'localhost',
+        'PORT':'5432',
+        
     }
 }
 
